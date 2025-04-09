2268f434f4e0edcc9e07550413b6076af7382ebc
NS180 <04nandinisharma@gmail.com>
2025-04-09 22:00:44 +0530

    Update my.h

--- my.h
+++ my.h
@@ -1,15 +1,58 @@
-// C program to use the above created header file 
-#include <stdio.h> 
-#include "myhead.h" 
-int main() 
-{ 
-	add(4, 6); 
 
-	/*This calls add function written in myhead.h 
-	and therefore no compilation error.*/
-	multiply(5, 5); 
+#ifndef MYMATHUTILS_H
+#define MYMATHUTILS_H
 
-	// Same for the multiply function in myhead.h 
-	printf("BYE!See you Soon"); 
-	return 0; 
-} 
+#include <stdio.h>
+#include <math.h>
+
+// Function to check if a number is an Armstrong number
+int armstrong(int n) {
+    int len = 0, temp = n, sum = 0, rem;
+    while (temp != 0) {
+        temp /= 10;
+        len++;
+    }
+    temp = n;
+    while (temp > 0) {
+        rem = temp % 10;
+        sum += pow(rem, len);
+        temp /= 10;
+    }
+    return (sum == n);
+}
+
+// Function to print Fibonacci series up to n terms
+int fibonacci(int n) {
+    int first = 0, second = 1, next, i;
+
+    printf("Fibonacci Series: ");
+    
+    for (i = 0; i < n; i++) {
+        if (i <= 1) {
+            next = i;
+        } else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d ", next);
+    }
+    printf("\n");
+    return 0;
+}
+
+// Function to print a number pattern
+int pattern(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2 * (n - i) - 1; j++) {
+            printf(" ");
+        }
+        for (int k = 0; k < 2 * i + 1; k++) {
+            printf("%d ", k + 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+#endif // MYMATHUTILS_H
